0d82652cc1cbffd9faa1e8287c234ace7403b2cc
vidman22 <29338715+vidman22@users.noreply.github.com>
2022-03-23 03:04:12 +0200

    new node resolve idk

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import nodePolyfill from 'rollup-plugin-polyfill-node'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
 import json from '@rollup/plugin-json'
@@ -31,14 +32,15 @@ export default {
       modules: true
     }),
     url(),
+    // node({crypto: true}),
     svgr(),
     babel({
       exclude: 'node_modules/**'
     }),
-    resolve({
-      preferBuiltins: true,
-      browser: true
+    nodeResolve({
+        browser: true,
     }),
+    nodePolyfill(),
     commonjs(),
     json()
   ],
